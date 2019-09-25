6052e170c8761e28c3ae25533b6f744c8330ddae
Jamie Watterson <jamiewatterson@catalyst.net.nz>
2019-09-25 14:59:41 +1200

    Moved logic out of Getuser and moved to parent App so that it could have
    shared state. Added text input and button for changing token.

--- MaharaMobile/App.tsx
+++ MaharaMobile/App.tsx
@@ -1,20 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Getuser from './components/Getuser.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={{height: 110, backgroundColor: 'grey', alignItems: 'center', justifyContent: 'flex-end'}}>
-        <Text style={{fontSize: 20, color: 'white', padding: 20}}>Mahara Mobile</Text>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stuff: '',
+      url: 'http://google.com',
+      loading: true,
+      token: ''
+    };
+  }
+
+  updateState = async (json) => {
+    const url = 'https://master.dev.mahara.org/';
+    const serverUrl = url + 'webservice/rest/server.php?alt=json';
+    const api = 'module_mobileapi_sync';
+
+    const body = {
+      blogs: {},
+      folders: {},
+      tags: {},
+      userprofile: {},
+      userprofileicon: {},
+      wsfunction: "module_mobileapi_sync",
+      wstoken: this.state.token
+    };
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    };
+
+    try {
+      const response = await fetch(serverUrl, requestOptions);
+      const json = await response.json();
+      this.receiveRequest(json);
+    } catch (error) {
+      this.errorHandle(error);
+    }
+  };
+
+  receiveRequest = (json) => {
+    this.setState({
+     loading: false,
+     stuff: 'Hi ' + json.userprofile.myname + '!'
+    });
+  }
+
+  errorHandle = (error) => {
+    this.setState({
+      stuff: 'Unable to connect to server'
+    })
+    console.log('errorr', error);
+  }
+
+  handler = (value) => {
+    console.log('hit');  
+    this.setState({
+      token: value
+    })
+    console.log(this.state.token);
+    this.updateState();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{height: 180, backgroundColor: 'grey', alignItems: 'center', justifyContent: 'flex-end'}}>
+          <Text style={{fontSize: 20, color: 'white'}}>Mahara Mobile</Text>
+          <Text style={{padding: 20}}>{this.state.stuff}</Text>
+        </View>
+        <View style={{flex: 1, backgroundColor: 'skyblue', alignItems: 'center', justifyContent: 'flex-start'}}>
+          <Getuser handler={this.handler} />
+        </View>
       </View>
-      <View style={{flex: 1, backgroundColor: 'skyblue', alignItems: 'center', justifyContent: 'flex-start'}}>
-        <Getuser />
-      </View>
-    </View>
-  );
-}
+    );
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
--- MaharaMobile/components/Getuser.js
+++ MaharaMobile/components/Getuser.js
@@ -1,78 +1,38 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default class Getuser extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      stuff: '',
-      url: 'http://google.com',
-      loading: true,
-      token: 'ab6f3b68f8b0b976cf6b51eac2cd54da',
-      pic: 'http://google.com'
+      token: ''
     };
-
-    updateState = async (json) => {
-      const url = 'https://master.dev.mahara.org/';
-      const serverUrl = url + 'webservice/rest/server.php?alt=json';
-      const api = 'module_mobileapi_sync';
-
-      const body = {
-        blogs: {},
-        folders: {},
-        tags: {},
-        userprofile: {},
-        userprofileicon: {},
-        wsfunction: "module_mobileapi_sync",
-        wstoken: "ab6f3b68f8b0b976cf6b51eac2cd54da"
-      };
-
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-      };
-
-      try {
-        const response = await fetch(serverUrl, requestOptions);
-        const json = await response.json();
-        receiveRequest(json);
-      } catch (error) {
-        errorHandle(error);
-      }
-    };
-
-    receiveRequest = (json) => {
-      console.log(json);
-
-      this.setState({
-       loading: false,
-       stuff: 'Hi ' + json.userprofile.myname,
-       pic: 'Pic: ' + json.userprofileicon.name,
-      });
-    }
-
-    errorHandle = (error) => {
-      this.setState({
-        stuff: 'Unable to connect to server'
-      })
-      console.log(error);
-    }
   };
 
-  componentDidMount() {
-    updateState();
+  updateToken = (input) => {
+    let token = input.trim();
+
+    this.setState({
+      token: token
+    })
+
+    console.log(this.state.token);
   }
 
   render() {
 
     return (
       <View style={{padding: 10, color: 'black'}}>
-        <Text>{this.state.pic}</Text>
-        <Text>{this.state.stuff}</Text>
+        <TextInput
+          style={styles.textInputStyle}
+          defaultValue='ab6f3b68f8b0b976cf6b51eac2cd54da'
+          onChangeText={(token) => this.updateToken(token)}
+        />
+        <Button
+          title="Verify token"
+          onPress={() => this.props.handler(this.state.token) }
+         />
       </View>
     )
   }
@@ -83,6 +43,7 @@ const styles = StyleSheet.create({
     color: 'green',
     paddingLeft: 20,
     paddingRight: 20,
+    marginBottom: 20,
     borderWidth: 1,
     borderColor: 'black',
     backgroundColor: 'white',
