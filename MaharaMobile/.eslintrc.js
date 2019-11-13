de14e5ae15447657916ccc335d400070dccec28c
Jamie Watterson <jamiewatterson@catalyst.net.nz>
2019-11-13 15:53:57 +1300

    Updated libraries to match Catalyst and fix npm run lint

--- MaharaMobile/.eslintrc.js
+++ MaharaMobile/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+  extends: ['prettier', 'airbnb', 'prettier/@typescript-eslint', 'prettier/react'],
   plugins: ['react', 'react-native'],
   "parserOptions": {
       "ecmaVersion": 6,
