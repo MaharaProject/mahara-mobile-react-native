fb5fb92529c97a4bf9edf85c5b06dfc7ee09a7cd
Doris Tam <doristam@catalyst.net.nz>
2019-11-14 18:01:18 +1300

    WIP Changes include:   - Adding an alert when not selecting a file when backing out of a file upload without selecting a file   - Changing dummy data out of PendingScreen which uses reducers and altered a model. (Needed to update AddScreen to add to UploadList)   - Network bug known

--- MaharaMobile/screens/AddScreen/AddScreen.tsx
+++ MaharaMobile/screens/AddScreen/AddScreen.tsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, Text, View, Image, ScrollView} from 'react-native';
+import { TouchableOpacity, Text, View, Image, ScrollView, Alert } from 'react-native';
 import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
 import { connect } from 'react-redux';
 
-import { uploadToMahara, uploadJournalToMahara } from '../../actions/actions';
+import { uploadToMahara, uploadJournalToMahara, updateUploadList } from '../../actions/actions';
 import Header from '../../components/Header/Header';
 import UploadForm from '../../components/UploadForm/UploadForm';
 import SelectMediaType from '../../components/SelectMediaType/SelectMediaType';
@@ -19,6 +19,7 @@ type Props = {
   token: string;
   dispatch: any;
   navigation: any;
+  uploadList: Array<MaharaFile>;
 };
 
 type State = {
@@ -38,7 +39,7 @@ type State = {
 const initialState = {
   title: '',
   description: '',
-  pickedFile: {uri: '', name: '', type: '', size: 0},
+  pickedFile: { uri: '', name: '', mimetype: '', size: 0 },
   pickedFolder: '',
   pickedBlog: 0,
   selectedTags: [],
@@ -47,7 +48,7 @@ const initialState = {
   webservice: 'module_mobileapi_upload_file',
   filePickerButtonText: 'Pick a file',
   mediaTypeHeader: ''
-}
+};
 
 export class AddScreen extends Component<Props, State> {
   constructor(props: Props) {
@@ -65,36 +66,36 @@ export class AddScreen extends Component<Props, State> {
       formType: type
     });
 
-    switch(type) {
+    switch (type) {
       case 'file':
         this.setState({
           filePickerButtonText: 'Pick a different file',
           mediaTypeHeader: 'Upload a file'
-        })
-        this.pickDocument()
-      break;
+        });
+        this.pickDocument();
+        break;
       case 'journal':
         this.setState({
           webservice: 'module_mobileapi_upload_blog_post',
           mediaTypeHeader: 'Add a journal entry'
-        })
-      break;
+        });
+        break;
       case 'photo':
         this.setState({
           mediaTypeHeader: 'Upload your photo'
-        })
-      break;
+        });
+        break;
       case 'audio':
         this.setState({
           mediaTypeHeader: 'Upload your recording'
-        })
-      break;
+        });
+        break;
     }
-  }
+  };
 
   resetForm = () => {
     this.setState(initialState);
-  }
+  };
 
   addTag = (tag: string) => {
     if (tag === 'Add new tag +') {
@@ -103,17 +104,17 @@ export class AddScreen extends Component<Props, State> {
       const selectedTags = [...this.state.selectedTags, tag];
       this.setState({ selectedTags, showTagInput: false });
     }
-  }
+  };
 
   removeTag = (tag: string) => {
-    const selectedTags = this.state.selectedTags.filter(item => item != tag);
+    const selectedTags = this.state.selectedTags.filter((item) => item != tag);
     this.setState({ selectedTags });
-  }
+  };
 
   setTagString = (tags: Array<string>) => {
     const tagsArray: Array<string> = [];
 
-    tags.map(function(tag, index) {
+    tags.map(function (tag, index) {
       tagsArray.push(tag + '&tags[' + (index + 1) + ']=');
     });
 
@@ -121,22 +122,28 @@ export class AddScreen extends Component<Props, State> {
     const string = '&tags[0]=' + tagsString;
 
     return string;
-  }
+  };
 
   pickDocument = async () => {
     // iPhone/Android
     DocumentPicker.show(
       {
-        filetype: [DocumentPickerUtil.allFiles()],
+        filetype: [DocumentPickerUtil.allFiles()]
       },
       (error, res) => {
         //error
-        console.log(error);
+        console.log('consolelog error:', error);
+
+        // No file picked
+        if (!res) {
+          Alert.alert('Invalid file', 'Please pick a file', [{ text: 'Okay', style: 'destructive' }]);
+          return;
+        }
 
         const pickedFile: MaharaFile = {
           name: res.fileName,
           uri: res.uri,
-          type: res.type,
+          mimetype: res.type,
           size: Number(res.fileSize)
         };
 
@@ -146,22 +153,24 @@ export class AddScreen extends Component<Props, State> {
         });
       }
     );
-  }
+  };
 
   setFormValue = (fieldName: string, value: string) => {
     const stateObject = () => {
       let returnObj: any = {};
       returnObj[fieldName] = value;
       return returnObj;
-    }
+    };
     this.setState(stateObject);
-  }
+  };
 
+  // Add files to uploadList
   handleForm = () => {
     const { selectedTags, pickedFile, pickedFolder, title, description } = this.state;
     const { userFolders, token } = this.props;
     let url = 'https://master.dev.mahara.org/webservice/rest/server.php?alt=json';
 
+    // Upload Journal Entry
     if (this.state.formType === 'journal') {
       const firstBlog = this.props.userBlogs[0].id;
       const journalEntry: JournalEntry = {
@@ -172,9 +181,10 @@ export class AddScreen extends Component<Props, State> {
         body: description,
         isdraft: false,
         tags: selectedTags
-      }
+      };
       this.props.dispatch(uploadJournalToMahara(url, journalEntry));
     } else {
+      // Upload File
       const tagString = selectedTags ? this.setTagString(selectedTags) : '';
       let url = 'https://master.dev.mahara.org/webservice/rest/server.php?alt=json' + tagString;
       const extension = pickedFile.name.match(/\.[0-9a-z]+$/i);
@@ -185,10 +195,15 @@ export class AddScreen extends Component<Props, State> {
 
       const fileData: MaharaFile = {
         uri: pickedFile.uri,
-        type: pickedFile.type,
+        mimetype: pickedFile.mimetype,
         name: pickedFile.name,
         size: pickedFile.size
       };
+
+      console.log('filedata', fileData);
+      // add to pending list but let og things happen anyway
+      this.props.dispatch(updateUploadList([fileData]));
+      console.log(this.props.uploadList)
 
       const formData = new FormData();
       formData.append('wsfunction', webservice);
@@ -202,28 +217,30 @@ export class AddScreen extends Component<Props, State> {
 
       this.props.dispatch(uploadToMahara(url, formData));
     }
-  }
+  };
 
   render() {
     return (
       <ScrollView>
         <Header navigation={this.props.navigation} />
         <View style={styles.view}>
-        {this.state.mediaTypeHeader ?
-          // TODO: temporary styling, add in header styles from diffent branch
-          <Text style={{fontSize: 24, fontWeight: 'bold', marginBottom: 20}}>{this.state.mediaTypeHeader}</Text>
-        : null}
-          {this.state.pickedFile.name ?
+          {this.state.mediaTypeHeader ? (
+            // TODO: temporary styling, add in header styles from diffent branch
+            <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20 }}>
+              {this.state.mediaTypeHeader}
+            </Text>
+          ) : null}
+          {this.state.pickedFile.name ? (
             <View style={styles.imageWrap}>
-              <Image source={{uri: this.state.pickedFile.uri}} style={styles.image} />
+              <Image source={{ uri: this.state.pickedFile.uri }} style={styles.image} />
             </View>
-          : null}
+          ) : null}
           <SelectMediaType
             selectAddType={this.selectAddType}
             formType={this.state.formType}
             filePickerButtonText={this.state.filePickerButtonText}
           />
-          {this.state.formType ?
+          {this.state.formType ? (
             <View>
               <UploadForm
                 pickedFile={this.state.pickedFile}
@@ -242,10 +259,10 @@ export class AddScreen extends Component<Props, State> {
                 <Text style={buttons.sm}>Back</Text>
               </TouchableOpacity>
             </View>
-          : null}
+          ) : null}
         </View>
       </ScrollView>
-    )
+    );
   }
 }
 
@@ -254,8 +271,9 @@ const mapStateToProps = (state: Store) => {
     token: state.app.token,
     userTags: state.app.userTags,
     userFolders: state.app.userFolders,
-    userBlogs: state.app.userBlogs
-  }
-}
+    userBlogs: state.app.userBlogs,
+    uploadList: state.app.uploadList
+  };
+};
 
 export default connect(mapStateToProps)(AddScreen);
