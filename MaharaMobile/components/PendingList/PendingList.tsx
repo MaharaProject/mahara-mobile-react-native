d65b4fbc64be583e39463416508e6c864604d450
Doris Tam <doristam@catalyst.net.nz>
2019-11-27 17:02:23 +1300

    fix tabs to 2

--- MaharaMobile/components/PendingList/PendingList.tsx
+++ MaharaMobile/components/PendingList/PendingList.tsx
@@ -5,50 +5,50 @@ import { MaharaPendingFile } from '../../models/models';
 import { Text } from 'react-native';
 
 const PendingList = (props: any) => {
-    return (
-        <FlatList
-            data={props.dataList}
-            extraData={props.selectedFiles}
-            renderItem={({ item }) => {
-                const uploadItem: any = item;
+  return (
+    <FlatList
+      data={props.dataList}
+      extraData={props.selectedFiles}
+      renderItem={({ item }) => {
+        const uploadItem: any = item;
 
-                if (props.uploadType === 'file') {
-                    const thumbnail = { uri: (uploadItem.maharaFormData.filetoupload.uri ? uploadItem.maharaFormData.filetoupload.uri : '') }
-                    return (
-                        <UploadItem
-                            itemId={uploadItem.id}
-                            title={uploadItem.maharaFormData.title}
-                            onRemove={() => props.onRemove(uploadItem.id)}
-                            onEdit={() => props.navigation.navigate({
-                                routeName: 'FileDetails',
-                                params: {
-                                    itemId: uploadItem.id
-                                },
-                            })}
-                            image={thumbnail}
-                        />
-                    );
-                } else if (props.uploadType === 'journalEntry') {
-                    return (
-                        <UploadItem
-                            itemId={uploadItem.id}
-                            title={uploadItem.journalEntry.title}
-                            onRemove={() => props.onRemove(uploadItem.id)}
-                            onEdit={() => props.navigation.navigate({
-                                routeName: 'FileDetails',
-                                params: {
-                                    itemId: uploadItem.id
-                                },
-                            })}
-                        // image={thumbnail}
-                        />
-                    );
-                }
-                return null
-            }
-            }
-        />
-    )
+        if (props.uploadType === 'file') {
+          const thumbnail = { uri: (uploadItem.maharaFormData.filetoupload.uri ? uploadItem.maharaFormData.filetoupload.uri : '') }
+          return (
+            <UploadItem
+              itemId={uploadItem.id}
+              title={uploadItem.maharaFormData.title}
+              onRemove={() => props.onRemove(uploadItem.id)}
+              onEdit={() => props.navigation.navigate({
+                routeName: 'FileDetails',
+                params: {
+                  itemId: uploadItem.id
+                },
+              })}
+              image={thumbnail}
+            />
+          );
+        } else if (props.uploadType === 'journalEntry') {
+          return (
+            <UploadItem
+              itemId={uploadItem.id}
+              title={uploadItem.journalEntry.title}
+              onRemove={() => props.onRemove(uploadItem.id)}
+              onEdit={() => props.navigation.navigate({
+                routeName: 'FileDetails',
+                params: {
+                  itemId: uploadItem.id
+                },
+              })}
+            // image={thumbnail}
+            />
+          );
+        }
+        return null
+      }
+      }
+    />
+  )
 }
 
 export default PendingList;
