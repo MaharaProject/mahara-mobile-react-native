577d56bb9067ab6f7ab934a7cfe50c58736f0316
Doris Tam <doristam@catalyst.net.nz>
2020-01-23 11:48:31 +1300

    Resolve "Find react-native compatible library for Camera"

--- MaharaMobile/screens/AddFileScreen/AddFileScreen.tsx
+++ MaharaMobile/screens/AddFileScreen/AddFileScreen.tsx
@@ -3,6 +3,8 @@ import { TouchableOpacity, Text, View, Image, ScrollView, Alert } from 'react-na
 import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
 import { connect } from 'react-redux';
 
+import ImagePicker from 'react-native-image-picker';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import UploadForm from '../../components/UploadForm/UploadForm';
 import AddAudio from '../../components/AddAudio/AddAudio';
 import styles from '../AddFileScreen/AddFileScreen.style';
@@ -39,15 +41,51 @@ const AddFileScreen = (props: Props) => {
   let isEditing = false;
   let formType = props.navigation.getParam('formType');
   const [pickedFile, setPickedFile] = useState<MaharaFile>(initialState);
-  const [filePickerButtonText, setFilePickerButtonText] = useState(props.navigation.getParam('item') ? 'Pick a different file' : 'Select a file');
+  const [filePickerButtonText, setFilePickerButtonText] = useState(props.navigation.getParam('itemToEdit') ? 'Pick a different file' : 'Select a file');
 
   // check if user is adding new or editing existing
   // populate form with existing details and set 'type' so headerTitle is accurate
-  if (props.navigation.getParam('item')) {
-    if (props.navigation.getParam('item').maharaFormData) {
-      initialState = props.navigation.getParam('item').maharaFormData.filetoupload;
+  if (props.navigation.getParam('itemToEdit')) {
+    if (props.navigation.getParam('itemToEdit').maharaFormData) {
+      initialState = props.navigation.getParam('itemToEdit').maharaFormData.filetoupload;
       isEditing = true;
     }
+  }
+
+  const takePhoto = () => {
+    const options = {
+      title: 'Select Image',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      }
+    };
+
+    /**
+     * The first arg is the options object for customization (it can also be null or omitted for default options),
+     * The second arg is the callback which sends object: response (more info in the API Reference)
+     */
+    ImagePicker.launchCamera(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+        Alert.alert('No photo captured', 'Camera closed by user');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+        Alert.alert(`ImagePicker Error:${response.error}`);
+      } else {
+        setPickedFile({
+          name: response.fileName,
+          uri: response.uri,
+          type: response.type,
+          size: Number(response.fileSize)
+        });
+
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+      }
+    });
   }
 
   useEffect(() => {
@@ -79,7 +117,7 @@ const AddFileScreen = (props: Props) => {
           type: res.type,
           size: Number(res.fileSize)
         });
-        setFilePickerButtonText('Pick a different file')
+        setFilePickerButtonText('Pick a different file');
       }
     );
   };
@@ -87,24 +125,29 @@ const AddFileScreen = (props: Props) => {
   return (
     <ScrollView>
       <View style={generic.wrap}>
-        {pickedFile.name && formType === 'file' ? (
+        {pickedFile.name && (formType === 'file' || formType === 'photo') ? (
           <View style={styles.imageWrap}>
             <Image source={{ uri: pickedFile.uri }} style={styles.image} />
           </View>
         ) : null}
-        {formType === 'file' &&
-        <View>
-          <TouchableOpacity onPress={() => pickDocument()}>
-            <Text style={buttons.lg}>{filePickerButtonText}</Text>
+        {formType === 'file' && 
+          <View>
+            <TouchableOpacity onPress={() => pickDocument()}>
+              <Text style={buttons.lg}>{filePickerButtonText}</Text>
+            </TouchableOpacity>
+          </View>
+        }
+        {formType === 'photo' &&
+          <TouchableOpacity onPress={() => takePhoto()}>
+            <Text style={buttons.lg}>
+              <FontAwesome5 name="camera" size={20}/>
+              &nbsp; Open Camera
+            </Text>
           </TouchableOpacity>
-        </View>
         }
         {formType === 'audio' &&
           <View>
-            <AddAudio
-              setPickedFile = {setPickedFile}
-              isEditing = {isEditing}
-            />
+            <AddAudio setPickedFile={setPickedFile} isEditing={isEditing} />
           </View>
         }
         <View>
@@ -116,7 +159,7 @@ const AddFileScreen = (props: Props) => {
             formType={formType}
             token={props.token}
             url={props.url}
-            editItem={props.navigation.getParam('item')}
+            editItem={props.navigation.getParam('itemToEdit')}
             navigation={props.navigation}
           />
         </View>
@@ -126,15 +169,15 @@ const AddFileScreen = (props: Props) => {
 }
 
 const setAddorEdit = (props: Props) => {
-  if (props.navigation.getParam('item')) {
-    return 'Edit'
-  } else {
-    return 'Add'
+  if (props.navigation.getParam('itemToEdit')) {
+    return 'Edit';
   }
-}
+  return 'Add';
+};
 
 AddFileScreen.navigationOptions = (props: Props) => ({
-  headerTitle: `${setAddorEdit(props)} ${props.navigation.getParam('formType')}`
+  headerTitle: `${setAddorEdit(props)} ${props.navigation.getParam('formType')}`,
+  headerLeft: null
 });
 
 const mapStateToProps = (state: RootState) => {
