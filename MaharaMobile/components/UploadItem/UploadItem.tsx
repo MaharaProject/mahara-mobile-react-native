fd68031aee480f62ccf48ca23e2a14a816edf021
Maia Miller <maiamiller@catalyst.net.nz>
2020-01-28 15:02:57 +1300

    Refactored PendingScreen to be functional component with hooks: * removed unused uploadRequest variables * uploadItemExist changed from boolean to count * created usePrev custom hook * when prevUploadCount < UploadCount, show success message

--- MaharaMobile/components/UploadItem/UploadItem.tsx
+++ MaharaMobile/components/UploadItem/UploadItem.tsx
@@ -17,7 +17,7 @@ type Props = {
   onRemove: () => {};
   onEdit: () => {};
   onClearError: () => {};
-}
+};
 
 const UploadItem = (props: Props) => {
   const title = (props.title ? props.title : '');
@@ -31,61 +31,60 @@ const UploadItem = (props: Props) => {
     mimetypes.forEach((type: string) => {
       if (mimetype.includes(type)) {
         match = type;
-        return;
       }
     });
 
     switch (match) {
       case 'application':
-        return 'file'
+        return 'file';
       case 'audio':
-        return 'music'
+        return 'music';
       case 'text':
-        return 'anchor'
+        return 'anchor';
       case 'video':
-        return 'film'
+        return 'film';
       case 'journalEntry':
-        return 'book'
+        return 'book';
       default:
-        return 'question'
+        return 'question';
     }
-  }
+  };
 
   const Thumbnail = () => {
     // not an image
-    if (!props.mimetype.includes('image')
-      // && !props.mimetype.includes('video')
-    ) {
+    if (!props.mimetype.includes('image')) {
       return (
         <View style={uploadItemStyles.imageContainer}>
-          <Icon name={getMimetypeIcon(props.mimetype)}
+          <Icon
+            name={getMimetypeIcon(props.mimetype)}
             size={30}
-            type='font-awesome'
+            type="font-awesome"
             color={styles.colors.primary}
             raised
             reverse
             reverseColor={styles.colors.secondary}
           />
-        </View>)
+        </View>
+      );
     }
-    else return (
+    return (
       <View style={[uploadItemStyles.imageContainer, { borderWidth: 4, borderColor: styles.colors.light }]}>
         <Image source={props.image} style={uploadItemStyles.thumbnail} />
       </View>
-    )
-  }
+    );
+  };
 
   return (
     <TouchableOpacity>
       <View style={uploadItemStyles.uploadItem}>
         <Card style={{ ...uploadItemStyles.pendingCard }}>
           {props.successfullyUploadedItem && <Text>Upload successful!</Text>}
-          {props.showUploadError &&
+          {props.showUploadError && (
             <View>
               <Text>There was an error uploading this file. Please try again.</Text>
-              <Icon onPress={props.onClearError} name="times" type="font-awesome" color={styles.colors.dark} />
+              <Icon onPress={props.onClearError} accessibilityLabel="Close error message" name="times" type="font-awesome" color={styles.colors.dark} />
             </View>
-          }
+          )}
           <Thumbnail />
           <View style={uploadItemStyles.textContainer}>
             <Text>{displayName} </Text>
@@ -94,11 +93,11 @@ const UploadItem = (props: Props) => {
           <View style={uploadItemStyles.buttonContainer}>
             <View style={uploadItemStyles.button}>
               <Button
-                title='Remove'
+                title="Remove"
                 onPress={props.onRemove}
                 color={styles.colors.primary}
-              // iOS styles
-              // backgroundColor={styles.colors.primary}
+                // iOS styles
+                // backgroundColor={styles.colors.primary}
               />
             </View>
 
@@ -107,15 +106,15 @@ const UploadItem = (props: Props) => {
                 title="Edit"
                 onPress={props.onEdit}
                 color={styles.colors.secondary}
-              // iOS styles
-              // backgroundColor={styles.colors.secondary}
+                // iOS styles
+                // backgroundColor={styles.colors.secondary}
               />
             </View>
           </View>
         </Card>
       </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 export default UploadItem;
