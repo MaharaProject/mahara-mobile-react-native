8d11c7ac45a56387e5ab5e264d68dc5e817f9122
Doris Tam <doristam@catalyst.net.nz>
2020-02-12 18:27:57 +1300

    Resolve "About page"

---
+++ MaharaMobile/src/screens/AboutScreen/AboutScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import generic from '../../assets/styles/generic';
+
+const AboutScreen = () => {
+
+  return (
+    <View style={generic.wrap}>
+      <Text>About Screen Text</Text>
+    </View>
+  );
+};
+
+export default AboutScreen;
