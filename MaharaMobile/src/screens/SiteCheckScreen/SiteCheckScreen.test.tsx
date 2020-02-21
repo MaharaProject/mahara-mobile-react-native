c0e48e9433c430308884615a51c32be962933b57
Jamie Watterson <jamiewatterson@catalyst.net.nz>
2020-02-21 13:02:32 +1300

    Fixed siteCheckScreen test

--- MaharaMobile/src/screens/SiteCheckScreen/SiteCheckScreen.test.tsx
+++ MaharaMobile/src/screens/SiteCheckScreen/SiteCheckScreen.test.tsx
@@ -1,12 +1,17 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { I18nProvider } from '@lingui/react';
 import { SiteCheckScreen } from './SiteCheckScreen';
+import { i18n } from '../../i18n';
 
 it('renders correctly', () => {
   let props = {
     url: ''
   };
 
-  renderer.create(<SiteCheckScreen {...props} />);
+  renderer.create(
+    <I18nProvider i18n={i18n} language="en">
+      <SiteCheckScreen {...props} />
+    </I18nProvider>);
 });
