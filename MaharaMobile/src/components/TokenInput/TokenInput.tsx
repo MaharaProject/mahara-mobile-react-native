213e5b3eccce859efdfb80b9d7bf54965072847f
Doris Tam <doristam@catalyst.net.nz>
2020-03-02 16:23:13 +1300

    managing data from guesting logging into user

--- MaharaMobile/src/components/TokenInput/TokenInput.tsx
+++ MaharaMobile/src/components/TokenInput/TokenInput.tsx
@@ -23,22 +23,22 @@ type Props = {
 };
 
 export default function TokenInput(props: Props) {
-  const [token, setToken] = useState('');
-
+  const [token, setToken] = useState('');    
+   
   return (
     <View style={styles.view}>
       <LinearGradient colors={[variables.colors.dark2, variables.colors.tertiary, variables.colors.light2]} style={generic.linearGradient}>
-      <View style={styles.wrapper}>
-        <View style={styles.imageWrapper}>
-          <LogoSvg />
-        </View>
+        <View style={styles.wrapper}>
+          <View style={styles.imageWrapper}>
+            <LogoSvg />
+          </View>
           <Text style={[headingStyles.mainHeading, generic.center]}><Trans>Log in via an access token</Trans></Text>
           <TextInput
             style={forms.textInput}
             // TODO: remove default value for go live
-            defaultValue="ab6f3b68f8b0b976cf6b51eac2cd54da"
+            defaultValue="1f1fa20d6ced9f281f663a9e6c1a30f8"
             onChangeText={(input) => setToken(input.trim())
-}
+            }
           />
           <MediumButton title={t`Verify token`} onPress={() => props.onUpdateToken(token)} />
         </View>
