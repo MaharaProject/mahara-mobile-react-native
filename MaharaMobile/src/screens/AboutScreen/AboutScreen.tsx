a42383f2aee6c9af27bcbedaf52f5c72a79b2e22
Doris Tam <doristam@catalyst.net.nz>
2020-03-03 10:34:25 +1300

    text additions and bug fixes

--- MaharaMobile/src/screens/AboutScreen/AboutScreen.tsx
+++ MaharaMobile/src/screens/AboutScreen/AboutScreen.tsx
@@ -1,5 +1,5 @@
 import {i18n} from '@lingui/core';
-import {t} from '@lingui/macro';
+import {t, Trans} from '@lingui/macro';
 import React from 'react';
 import {Text, View} from 'react-native';
 import generic from '../../assets/styles/generic';
@@ -8,7 +8,9 @@ import styles from '../../assets/styles/variables';
 const AboutScreen = () => {
   return (
     <View style={generic.wrap}>
-      <Text>About Screen Text</Text>
+      <Text>
+        <Trans>...</Trans>
+      </Text>
     </View>
   );
 };
