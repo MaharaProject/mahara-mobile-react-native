0d2f965d09f91464d1f51ca6389b98968bb8d504
Jamie Watterson <jamiewatterson@catalyst.net.nz>
2020-03-04 10:51:54 +1300

    Added Sentry to project.

--- MaharaMobile/src/App.tsx
+++ MaharaMobile/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Sentry from '@sentry/react-native';
 import {Provider} from 'react-redux';
 import {Buffer} from 'buffer';
 import {I18nProvider} from '@lingui/react';
@@ -8,6 +9,11 @@ import AppNavigator from './navigations/app-navigator';
 import i18n from './i18n';
 
 global.Buffer = Buffer;
+
+Sentry.init({
+  dsn: 'https://abfd5ab9b2134d30a5e9506c2a8d1de4@sentry.catalyst.net.nz/195'
+});
+Sentry.setTag('mobile-app', '2');
 
 const App = () => {
   const store = configureStore(undefined, i18n);
