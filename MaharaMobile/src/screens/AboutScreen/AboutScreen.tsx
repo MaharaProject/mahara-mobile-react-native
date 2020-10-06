e216811ee31da0d16c15d427070b722258c2c514
Kristina Hoeppner <kristina@catalyst.net.nz>
2020-10-07 10:46:41 +1300

    Resolve "Change text on the 'About' screen"

--- MaharaMobile/src/screens/AboutScreen/AboutScreen.tsx
+++ MaharaMobile/src/screens/AboutScreen/AboutScreen.tsx
@@ -15,45 +15,51 @@ const AboutScreen = () => {
         <ListItem style={{flexDirection: 'column'}}>
           <MediumText>
             <MediumText
-              text={t`Mahara Mobile is an open source project created by the Mahara
-            Project Team. To contribute or report bugs, see our`}
+              text={t`Mahara Mobile is an open source project created by the
+                Mahara Project Team. To contribute or report bugs, see our`}
             />
             <LinkText
               url="https://github.com/MaharaProject/mahara-mobile"
-              text={t`Mahara Project.`}
+              text={t`Mahara Project`}
             />
+            <MediumText text={t`repository on GitHub.`} />
           </MediumText>
         </ListItem>
         <ListItem>
           <MediumText
-            text={t`Mahara Mobile allows you to connect your mobile device to your
-            Mahara site. You can create and collect your content on your mobile
-            device and use the app to upload it to your Mahara site. You can any
-            file available for uploading via Android and create journal entries.`}
+            text={t`Mahara Mobile allows you to connect your mobile device to
+              your Mahara site. You can create and collect your content on your
+              mobile device and use the app to upload it to your Mahara site.
+              You can upload any files that your device allows you to upload
+              and create journal entries.`}
           />
         </ListItem>
         <ListItem>
           <MediumText
-            text={t`Mahara is an open source ePortfolio system designed for social
-            learning. An ePortfolio is a system in which learners can record
-            evidence of their learning, e.g. essays, artwork, certificates,
-            reflections or other such things they produce that can be stored
-            digitally.`}
+            text={t`Mahara is an open source ePortfolio platform designed for
+              reflecing on what you have learned and for supporting social
+              learning. An ePortfolio is a tool and method to record your
+              evidence of their learning, e.g. essays, artwork, and
+              certificates, and reflect on them to learn from your experiences
+              . You can also engage in conversations with others and create
+              group portfolios.`}
           />
         </ListItem>
         <ListItem>
           <MediumText
-            text={t`Mahara Mobile allows you to collect your evidence on your mobile
-            device and also when you are offline. You can then upload it to
-            Mahara and put into your portfolios.`}
+            text={t`Mahara Mobile allows you to collect your evidence on your
+              mobile device, including when you are offline. You can then
+              upload your photos, videos, journal entries, etc. to your Mahara
+              instance and add them your portfolios.`}
           />
         </ListItem>
         <ListItem>
           <MediumText
-            text={t`In order to push content to Mahara from Mahara Mobile, you require
-            an account on a Mahara site that allows mobile uploads. Typically,
-            the institution that you are affiliated with will make a Mahara
-            instance available to you to use if you are working with portfolios.`}
+            text={t`In order to push content to Mahara from Mahara Mobile, you
+              require an account on a Mahara site that allows mobile uploads.
+              Typically, the institution that you are affiliated with will make
+              a Mahara site available to you to use if you are working with
+              portfolios.`}
           />
         </ListItem>
       </List>
