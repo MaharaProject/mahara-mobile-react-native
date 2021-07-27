4ca3f76905deaa8f064070337b33c2004e9f75bd
Doris Tam <doristam@catalyst.net.nz>
2021-07-28 09:20:49 +1200

    Clean up for 21.04.2 ios

--- MaharaMobile/ios/File.swift
+++ MaharaMobile/ios/File.swift
@@ -1,7 +1,7 @@
 //
 //  File.swift
 //  MaharaMobile
-//
+//  Dummy file required for Bridging for Objective-C
 //  Created by Doris Tam on 27/07/21.
 //  Copyright © 2021 Catalyst .Net Limited. All rights reserved.
 //
