ecd11441210dba7596d2df0b16bf8e778d93b734
Doris Tam <doris.tam@catalyst.net.nz>
2021-09-13 02:24:36 +0000

    Resolve "Manage versioning better"

--- MaharaMobile/src/screens/VersionScreen/VersionScreen.tsx
+++ MaharaMobile/src/screens/VersionScreen/VersionScreen.tsx
@@ -2,11 +2,12 @@ import {t} from '@lingui/macro';
 import {List, ListItem} from 'native-base';
 import React from 'react';
 import MediumText from '../../components/UI/CustomText/MediumText';
+import {MAHARAVERSION} from '../../utils/constants';
 
 const VersionScreen = () => (
   <List>
     <ListItem noBorder>
-      <MediumText text={t`This version of Mahara Mobile is 21.04.2`} />
+      <MediumText text={t`This version of Mahara Mobile is ${MAHARAVERSION}`} />
     </ListItem>
   </List>
 );
