20f983c6b5bdd5c4304fad03ec3ed86e48869026
Doris Tam <doristam@catalyst.net.nz>
2021-10-11 14:25:21 +1300

    Swift compatibility patch fix

--- MaharaMobile/ios/File.swift
+++ MaharaMobile/ios/File.swift
@@ -1,8 +1,8 @@
 //
 //  File.swift
 //  MaharaMobile
-//  Dummy file required for Bridging for Objective-C
-//  Created by Doris Tam on 27/07/21.
+//
+//  Created by Doris Tam on 11/10/21.
 //  Copyright © 2021 Catalyst .Net Limited. All rights reserved.
 //
 
