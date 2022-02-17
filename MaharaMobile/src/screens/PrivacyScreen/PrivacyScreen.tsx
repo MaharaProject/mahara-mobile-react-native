d57538569eafbde98d2fb7bf43e605c48b8f9423
Doris Tam <doristam@catalyst.net.nz>
2022-02-17 17:07:48 +1300

    Recompile language strings and fix lint error

--- MaharaMobile/src/screens/PrivacyScreen/PrivacyScreen.tsx
+++ MaharaMobile/src/screens/PrivacyScreen/PrivacyScreen.tsx
@@ -85,10 +85,11 @@ const PrivacyScreen = () => (
         <Body>
           <SubHeading text={t`Deleting your data`} />
           <MediumText
-            text={t`Once you have uploaded data to your Mahara account,
-            it is no longer stored in the app.
+            text={t`Once you have uploaded data to your Mahara account, it is no longer stored in the app.
             Log into your account on the Mahara website you use to manage or delete uploaded data there.
-            When you log out of Mahara Mobile, all content you created in the app will be deleted. This cannot be undone. Only files that already existed on your device before you added them to your Mahara Mobile queue for uploading to your Mahara account will still be available on your device unless you deleted them yourself.`}
+            When you log out of Mahara Mobile, all content you created in the app will be deleted. This cannot be undone.
+            Only files that already existed on your device before you added them to your Mahara Mobile queue for uploading
+             to your Mahara account will still be available on your device unless you deleted them yourself.`}
           />
         </Body>
       </ListItem>
