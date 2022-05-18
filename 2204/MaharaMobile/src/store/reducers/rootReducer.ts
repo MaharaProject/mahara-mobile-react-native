3bf251ea3ba36955196a3e39d83187d9eae30d67
Doris Tam <doristam@catalyst.net.nz>
2022-05-18 14:16:05 +1200

    Add the store and install redux

---
+++ 2204/MaharaMobile/src/store/reducers/rootReducer.ts
@@ -0,0 +1,26 @@
+import {combineReducers} from 'redux';
+import {appSettingsReducer} from './appSettingsReducer';
+import {loginInfoReducer} from './loginInfoReducer';
+import {uploadFilesReducer} from './uploadFilesReducer';
+import {uploadJEntriesReducer} from './uploadJEntriesReducer';
+import {userBlogsReducer, userFoldersReducer} from './userArtefactsReducer';
+import {userTagsReducer} from './userTagsReducer';
+
+// Reducers specify how the application's state changes in response to actions sent to the store.
+export const rootReducer = combineReducers({
+  domainData: combineReducers({
+    loginInfo: loginInfoReducer,
+    userTagsInfo: userTagsReducer,
+    userFolders: userFoldersReducer,
+    userBlogs: userBlogsReducer
+  }),
+  appState: combineReducers({
+    uploadFiles: uploadFilesReducer,
+    uploadJEntries: uploadJEntriesReducer
+  }),
+  uiState: combineReducers({
+    appSettings: appSettingsReducer
+  })
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
