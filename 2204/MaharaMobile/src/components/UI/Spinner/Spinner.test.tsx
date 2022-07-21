9b0fe9d0fd886e99c4c937641b6f2277e7afd40c
Doris Tam <doristam@catalyst.net.nz>
2022-07-21 23:19:13 +1200

    remove 2204 folder and use it as primary

--- 2204/MaharaMobile/src/components/UI/Spinner/Spinner.test.tsx
+++
@@ -1,8 +0,0 @@
-import 'react-native';
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Spinner from './Spinner';
-
-it('renders correctly', () => {
-  renderer.create(<Spinner />);
-});
--- 2204/MaharaMobile/src/models/userArtefactsTypes.ts
+++
@@ -1,8 +0,0 @@
-import {UserFolder, UserBlog} from './models';
-
-export type UserFoldersActions = {
-  type: string;
-  userFolders: Array<UserFolder>;
-};
-
-export type UserBlogsActions = {type: string; userBlogs: Array<UserBlog>};
