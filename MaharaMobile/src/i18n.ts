cd42849334c79e0993c63889fb1630ecb10a4812
Bryn Bennett <brynbennett@catalyst.net.nz>
2022-08-17 10:48:22 +1200

    Translations: add plurals

--- MaharaMobile/src/i18n.ts
+++ MaharaMobile/src/i18n.ts
@@ -1,9 +1,14 @@
 import { i18n } from '@lingui/core';
+import { en, ko } from 'make-plural';
 import catalogEn from '../locales/en/messages';
 import catalogKo from '../locales/ko/messages';
 
-i18n.load('en', catalogEn.messages);
-i18n.load('ko', catalogKo.messages);
+i18n.loadLocaleData({
+  en: { plurals: en },
+  ko: { plurals: ko }
+});
+
+i18n.load({ en: catalogEn.messages, ko: catalogKo.messages });
 
 export const changeActiveLanguage = (newActiveLanguage: string) => {
   i18n.activate(newActiveLanguage);
