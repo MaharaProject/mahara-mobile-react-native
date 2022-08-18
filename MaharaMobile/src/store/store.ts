1972e51378963ff155fdcab2186cc051780d5f20
Bryn Bennett <brynbennett@catalyst.net.nz>
2022-08-18 12:23:22 +1200

    react-logger only use in dev (bad performance)

--- MaharaMobile/src/store/store.ts
+++ MaharaMobile/src/store/store.ts
@@ -1,10 +1,14 @@
-import { configureStore as reduxConfigureStore } from '@reduxjs/toolkit';
-import { createLogger } from 'redux-logger';
+import { Middleware, configureStore as reduxConfigureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './reducers/rootReducer';
+
+const devMiddleware: Middleware[] = [];
+if (__DEV__) {
+  import('redux-logger').then(({ createLogger }) => devMiddleware.push(createLogger()));
+}
 
 export default function configureStore() {
   return reduxConfigureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(createLogger())
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(devMiddleware)
   });
 }
