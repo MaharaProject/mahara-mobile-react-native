2ec39fd9e58a1ebff96ceb13aba92dab515a6353
Doris Tam <doristam@catalyst.net.nz>
2022-12-31 01:17:56 +1300

    Additional changes for french
    - add language to .linguirc
    - add language to i18n.ts
    - along with extraction and compliation

--- MaharaMobile/src/i18n.ts
+++ MaharaMobile/src/i18n.ts
@@ -1,14 +1,16 @@
 import { i18n } from '@lingui/core';
-import { en, ko } from 'make-plural';
+import { en, fr, ko } from 'make-plural';
 import catalogEn from '../locales/en/messages';
+import catalogFr from '../locales/fr/messages';
 import catalogKo from '../locales/ko/messages';
 
 i18n.loadLocaleData({
   en: { plurals: en },
+  fr: { plurals: fr },
   ko: { plurals: ko }
 });
 
-i18n.load({ en: catalogEn.messages, ko: catalogKo.messages });
+i18n.load({ en: catalogEn.messages, fr: catalogFr.messages, ko: catalogKo.messages });
 
 export const changeActiveLanguage = (newActiveLanguage: string) => {
   i18n.activate(newActiveLanguage);
